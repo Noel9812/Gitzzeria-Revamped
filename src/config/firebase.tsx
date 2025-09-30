e3a840b9559a30e8b75d31e95617c2bb5bfff544
Noel Mathews <106422418+Noel9812@users.noreply.github.com>
2025-09-30 13:21:57 +0530

    added .env file

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,28 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyATOObOlxBkm71oD-kKdO2V4TflKedvEeM",
-    authDomain: "gittzeria.firebaseapp.com",
-    projectId: "gittzeria",
-    storageBucket: "gittzeria.appspot.com",
-    messagingSenderId: "498702583979",
-    appId: "1:498702583979:web:ffe2a5e06e011a8f7c4fee",
-    measurementId: "G-Z842GXFQX7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
- const auth = getAuth(app);
+const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 
-
-export { db , auth };
+export { db, auth };
